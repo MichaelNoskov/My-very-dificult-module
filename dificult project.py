830311827e03d6d7b6232a399d716da64b3950d2
MaybeCaat <iemm@rambler.ru>
2022-10-28 15:59:48 +0300

    commit for Misha

--- dificult project.py
+++ dificult project.py
@@ -1,2 +1,6 @@
-def hello(name):
-	print('Hello, World from {name}')
+def hello_with_name(name):
+	return print(f'Hello, World from {name}!')
+
+
+if __name__ == '__main__':
+        hello_with_name('Misha')
